bba12a0f21aee7545465784be6ad20da67d3342e
Phil Hanna <ph1204@gmail.com>
2024-12-24 23:49:48 -0500

    Refactored the AgeBasedRating function

--- player.go
+++ player.go
@@ -19,19 +19,29 @@ type Player struct {
 // ---------------------------------------------------------------------
 
 // AgeBasedRating returns the provisional rating described in Section
-// 2.3 of the algorithm.
+// 2.3 of the algorithm.  The full algorithm handles the case where the
+// player is not rated in the basic USCF system but *is* rated in one
+// of the other systems, like FIDE or Canada.  But this version does not
+// implement those cases, using instead only the player's age.
 func AgeBasedRating(age int) float64 {
+	var rating float64
+
+	// If the age is not specified (i.e., age == 0) then assume the age
+	// is 26.  We do not currently support the case of the unrated player
+	// being a youth.
 	if age == 0 {
 		age = 26
 	}
-	var rating float64
+
+	// Now calculate the rating based on which slot the age fits in.
 	switch {
 	case age < 2:
-		rating = 100
-	case age >= 2 && age <= 26:
+		rating = 100.0
+	case age <= 26:
 		rating = float64(age) * 50
 	default:
-		rating = 1300
+		rating = 1300.0
 	}
+
 	return rating
 }
