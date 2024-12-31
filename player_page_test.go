8a49beb84ee89adecc668605d45c0853fa4cc7e8
Phil Hanna <ph1204@gmail.com>
2024-12-30 22:25:29 -0500

    Moved unit test

--- player_page_test.go
+++ player_page_test.go
@@ -15,28 +15,6 @@ func localGetPage(USCFID string) (string, error) {
 	body, err := os.ReadFile(fmt.Sprintf("testdata/%s.html", USCFID))
 	return string(body), err
 }
-
-func TestAgeBasedRating(t *testing.T) {
-	tests := []struct {
-		name string
-		age  int
-		want float64
-	}{
-		{age: 10, want: 500},
-		{age: 1, want: 100},
-		{age: 26, want: 1300},
-		{age: 70, want: 1300},
-		{age: 0, want: 1300},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			want := tt.want
-			have := AgeBasedRating(tt.age)
-			assert.Equal(t, want, have)
-		})
-	}
-}
-
 func TestBuildURL(t *testing.T) {
 	want := "https://www.uschess.org/msa/MbrDtlMain.php?15218438"
 	have := BuildURL(MAGNUS_CARLSEN)
--- player_test.go
+++ player_test.go
@@ -1,1 +1,29 @@
 package uscfratings
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAgeBasedRating(t *testing.T) {
+	tests := []struct {
+		name string
+		age  int
+		want float64
+	}{
+		{age: 10, want: 500},
+		{age: 1, want: 100},
+		{age: 26, want: 1300},
+		{age: 70, want: 1300},
+		{age: 0, want: 1300},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			want := tt.want
+			have := AgeBasedRating(tt.age)
+			assert.Equal(t, want, have)
+		})
+	}
+}
+
